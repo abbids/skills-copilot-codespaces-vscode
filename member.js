e5d23075ccdd1f206a57d459a4bf1b432d973be6
Sam <abbid_siddiqui@hotmail.com>
2024-03-18 00:38:07 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  return {
+    name: 'skillsMember',
+    type: 'MemberExpression',
+    message: 'MemberExpression',
+    description: 'MemberExpression',
+    category: 'MemberExpression',
+    recommended: true,
+    url: 'https://eslint.org/docs/rules/member',
+  };
+}
